02f27b87b58200fffd2fdd79c7d506820e47a965
pronad1 <ug2102049@cse.pstu.ac.bd>
2026-08-12 13:56:33 +0600

    R

--- hello.R
+++ hello.R
@@ -1,3 +1,11 @@
 name <- "Prosenjit Mondol"
 
 print(paste("Hello,", name, "!"))
+
+
+"Hello World!"
+5 + 5
+
+# direct plotting in a pdf
+
+plot(1:10, 1:10, type = "l")
--- simple.R
+++ simple.R
@@ -4,3 +4,11 @@ y <- 20
 result <- x + y
 
 print(result)
+
+# In R, variables do not need to be declared with any particular type, and can even change type after they have been set:
+
+s <- "Hello, World!" # s is type of character
+print(s)
+s <- result # s is now type of numeric
+print(s)
+
